f2a1b1cf53903eb098d1c3e6a86575eeb567987a
NuclearRedeye <j.summerfield@elifesciences.org>
2021-09-24 08:57:09 +0100

    chore: define skill labels

---
+++ src/labels/skills.ts
@@ -0,0 +1,85 @@
+import { Label } from '../interfaces/label';
+
+const color = '9448dc';
+
+export const skills: Label[] = [
+  {
+    name: 'html',
+    description: 'Solution requires expertise in HTML',
+    color,
+    alias: ['HTML']
+  },
+  {
+    name: 'css',
+    description: 'Solution requires expertise in CSS',
+    color,
+    alias: ['CSS']
+  },
+  {
+    name: 'javascript',
+    description: 'Solution requires expertise in JavaScript',
+    color,
+    alias: ['js']
+  },
+  {
+    name: 'typescript',
+    description: 'Solution requires expertise in TypeScript',
+    color,
+    alias: ['ts']
+  },
+  {
+    name: 'python',
+    description: 'Solution requires expertise in Python',
+    color,
+    alias: ['py']
+  },
+  {
+    name: 'php',
+    description: 'Solution requires expertise in PHP',
+    color,
+    alias: ['PHP']
+  },
+  {
+    name: 'docker',
+    description: 'Solution requires expertise in Docker',
+    color,
+    alias: []
+  },
+  {
+    name: 'jenkins',
+    description: 'Solution requires expertise in Jenkins',
+    color,
+    alias: []
+  },
+  {
+    name: 'aws',
+    description: 'Solution requires expertise in Amazon Web Services',
+    color,
+    alias: []
+  },
+  {
+    name: 'kubernetes',
+    description: 'Solution requires expertise in Kubernetes',
+    color,
+    alias: []
+  },
+  {
+    name: 'salt',
+    description: 'Solution requires expertise in SaltStack',
+    color,
+    alias: []
+  },
+  {
+    name: 'elasticsearch',
+    description: 'Solution requires expertise in Elasticsearch',
+    color,
+    alias: []
+  },
+  {
+    name: 'react',
+    description: 'Solution requires expertise in React',
+    color,
+    alias: []
+  },
+];
+
