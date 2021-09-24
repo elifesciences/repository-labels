bdb9dca05f92bdd204d8ecc570cdd2039f152861
NuclearRedeye <j.summerfield@elifesciences.org>
2021-09-24 08:56:38 +0100

    chore: define goal labels

---
+++ src/labels/goals.ts
@@ -0,0 +1,42 @@
+import { Label } from '../interfaces/label';
+
+const color = '76d7c4';
+
+export const goals: Label[] = [
+  {
+    name: 'edi',
+    description: 'Improve equity, diversity and inclusion across the organisation',
+    color,
+    alias: []
+  },
+  {
+    name: 'public review',
+    description: 'Achieve 90%+ preprint deposition at peer review',
+    color,
+    alias: []
+  },
+  {
+    name: 'readership',
+    description: 'Increase readship of research articles by 50%',
+    color,
+    alias: []
+  },
+  {
+    name: 'maintenance',
+    description: 'Ensure current state and functionality is preserved',
+    color,
+    alias: []
+  },
+  {
+    name: 'submissions',
+    description: 'Attract between 2500 and 3500 submissions per quarter',
+    color,
+    alias: []
+  },
+  {
+    name: 'sustainability',
+    description: 'Demonstrate financially sustainable core publishing operations',
+    color,
+    alias: []
+  },
+];
